8f7839c26691624eeb87fccb4e88b37f3265bcfc
Sam O'Cain <samocain@Sams-MBP.lan>
2022-11-26 00:06:34 -0600

    setting up main js

---
+++ main.js
@@ -0,0 +1,10 @@
+const colors = ["red", "green", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn');
+const color = document.getElementById('color-span')
+
+// Event Listeners
+btn.addEventListener("click", function() {
+    
+
+    const randomNum = 2;
+})
